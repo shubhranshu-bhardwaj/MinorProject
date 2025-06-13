5a1fc1a506956cee99ea92de4363942896e969ec
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2025-06-13 14:30:19 +0530

    Update SearchBar.tsx

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,10 +1,12 @@
-import './SearchBar.css'
+import './SearchBar.css';
 
-const SearchBar = ({onSearch}: {onSearch:(query:string) => void}) => 
-   (
-    <input type="text" className='search-bar' placeholder='Search job title, company, location'
-    onChange={(e)=>onSearch(e.target.value)} />
-  )
+const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => (
+  <input
+    type="text"
+    className="search-bar"
+    placeholder="Search by title, company, or tech stack"
+    onChange={(e) => onSearch(e.target.value)}
+  />
+);
 
-
-export default SearchBar
+export default SearchBar;
