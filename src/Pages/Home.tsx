d514a934e41d48387311387770a8f6ab43adfdfd
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2025-06-16 08:03:03 +0530

    Update Home.tsx

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,33 +1,60 @@
-import JobCard from "../Components/JobCard";
-import SearchBar from "../Components/SearchBar";
-import ThemeToggle from "../Components/ThemeToggle";
-import { useJob } from "../Hook/useJob";
-import "./Home.css";
-import logo from "./logo.png";
+import JobCard from '../Components/JobCard';
+import SearchBar from '../Components/SearchBar';
+import ThemeToggle from '../Components/ThemeToggle';
+import TopCompanies from '../Components/TopCompanies';
+import { useJobs } from '../Hook/useJobs';
+import './Home.css';
 
 const Home = () => {
-  const { jobs, loading, filterJobs, nextPage, prevPage, page, totalPages } = useJob();
+  const {
+    jobs,
+    allFiltered,
+    filtered,
+    loading,
+    filterJobs,
+    nextPage,
+    prevPage,
+    page,
+    totalPages
+  } = useJobs();
 
   if (loading) return <p>Loading jobs...</p>;
 
   return (
     <>
       <div className="top-bar">
-        <img src={logo} alt="Logo" className="logo" />
-        <SearchBar onSearch={filterJobs} />
-        <ThemeToggle />
+        <div className="nav-left">
+          <img src="/logos/jobfinder-logo.png" alt="JobFinder Logo" className="logo-img" />
+        </div>
+        <div className="nav-center">
+          <SearchBar onSearch={filterJobs} />
+        </div>
+        <div className="nav-right">
+          <ThemeToggle />
+        </div>
       </div>
 
-      <div className="home">
-        {jobs.map(job => (
-          <JobCard key={job.id} job={job} />
-        ))}
-      </div>
-
-      <div className="pagination">
-        <button disabled={page === 1} onClick={prevPage}>Previous</button>
-        <span>Page {page} of {totalPages}</span>
-        <button disabled={page === totalPages} onClick={nextPage}>Next</button>
+      <div className="content">
+        {filtered && (
+          <div className="search-section">
+            <h2>Search Result</h2>
+            {allFiltered.length === 0
+              ? <p>No matched found</p>
+              : <div className="home">{jobs.map(job => <JobCard key={job.id} job={job} />)}</div>}
+          </div>
+        )}
+        <TopCompanies />
+        <h2 className="section-title">Featured Jobs</h2>
+        <div className="home">
+          {jobs.map((job) => (
+            <JobCard key={job.id} job={job} />
+          ))}
+        </div>
+        <div className="pagination">
+          <button disabled={page === 1} onClick={prevPage}>Previous</button>
+          <span>Page {page} of {totalPages}</span>
+          <button disabled={page === totalPages} onClick={nextPage}>Next</button>
+        </div>
       </div>
     </>
   );
